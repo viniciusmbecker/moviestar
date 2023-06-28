33211e1c179277fc1dafc66a5741edc6b63312d8
viniciusmbecker <becker_vinicius@sicredi.com.br>
2023-06-27 23:49:55 -0300

    edição dos filmes

--- back/movie_process.php
+++ back/movie_process.php
@@ -109,11 +109,105 @@
       } else {
         
         $message->setMessage("Informações inválidas", "error", "index.php");
+
       }
       
 
-    } else {
-        
+    } else if($type === "update") { 
+
+        // Receber os dados dos inputs
+        $title = filter_input(INPUT_POST, "title");
+        $description = filter_input(INPUT_POST, "description");
+        $trailer = filter_input(INPUT_POST, "trailer");
+        $category = filter_input(INPUT_POST, "category");
+        $length = filter_input(INPUT_POST, "length");
+        $id = filter_input(INPUT_POST, "id");
+    
+        $movieData = $movieDao->findById($id);
+    
+        // Verifica se encontrou o filme
+        if($movieData) {
+    
+          // Verificar se o filme é do usuário
+          if($movieData->users_id === $userData->id) {
+    
+            // Validação mínima de dados
+            if(!empty($title) && !empty($description) && !empty($category)) {
+    
+              // Edição do filme
+              $movieData->title = $title;
+              $movieData->description = $description;
+              $movieData->trailer = $trailer;
+              $movieData->category = $category;
+              $movieData->length = $length;
+    
+              // Upload de imagem do filme
+              if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
+    
+                $image = $_FILES["image"];
+                $imageTypes = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+                $jpgArray = ["image/jpeg", "image/jpg"];
+                $pngArray = ["image/png"];
+    
+                // Checando tipo da imagem
+                if(in_array($image["type"], $imageTypes)) {
+    
+                  // Checagem se jpg
+                  if (in_array($image["type"], $jpgArray)) {
+                        
+                    $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+
+                  // Imagem é png    
+                  } elseif (in_array($image["type"], $pngArray)) {
+                    
+                    $imageFile = imagecreatefrompng($image["tmp_name"]);
+                    
+                  } else {
+
+                    $imageFile = imagecreatefromwebp($image["tmp_name"]);
+
+                  }
+    
+                  // Gerando o nome da imagem
+                  $movie = new Movie();
+    
+                  $imageName = $movie->imageGenerateName();
+    
+                  imagejpeg($imageFile, "./img/movies/" . $imageName, 100);
+    
+                  $movieData->image = $imageName;
+    
+                } else {
+    
+                  $message->setMessage("Tipo inválido de imagem, insira png ou jpg!", "error", "back");
+    
+                }
+    
+              }
+    
+              $movieDao->update($movieData);
+    
+            } else {
+    
+              $message->setMessage("Você precisa adicionar pelo menos: título, descrição e categoria!", "error", "back");
+    
+            }
+    
+          } else {
+    
+            $message->setMessage("Informações inválidas!", "error", "index.php");
+    
+          }
+    
+        } else {
+    
+          $message->setMessage("Informações inválidas!", "error", "index.php");
+    
+        }
+      
+      } else {
+    
         $message->setMessage("Informações inválidas!", "error", "index.php");
-    }
+    
+      }
     
